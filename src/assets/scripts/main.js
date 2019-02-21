7690ad8c1a96b25151d9421e0d7e118648320c7a
Alexander Varnaev <a.varnaev@dataduck.com>
2019-02-21 12:12:52 +0300

    show content without clean

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -4,62 +4,157 @@ const registration = document.querySelector(".registration"),
     content = document.querySelector(".content"),
     platform = document.querySelector(".platform"),
     goals = document.querySelectorAll(".form__checkbox"),
-    formWithGoals = document.querySelector('.form'),
-    template = document.querySelector('#content-tmp').innerHTML;
+    formWithGoals = document.querySelector('.form');
 
-const showContent = openHiddenContent();
+let goalsChecked = [];
+const showContent = createContent();
+
+for (let i=0; i<goals.length; i++){ 
+    goalsChecked.push(goals[i]);
+  }
 
 hideContent();
+createContent();
 
-goals.forEach(function(goal, i) {
-    console.log(goal, i);
-    let goalsChecked = [];
-    console.log(goalsChecked);
-    goalsChecked.push(goal[i]);
+goals.forEach(function(goal) {
     
-    goal.addEventListener("click", () => {
-        if (!goal.checked == false && goal.id =='iphone') {
+    goal.addEventListener("change", () => {
+        if (goal.id =='iphone' && goalsChecked[1].checked == false  && goalsChecked[2].checked == false) {
+            showContent.clear();
             showContent.open();
-            showContent.setChoise('iphone');
-            showContent.setPrize('iphone');
-            showContent.setText('iphone');
+            showContent.setChoise('Iphone Xs');
+            showContent.setPrize('Цена 1000$');
+            showContent.setText('Цена для трейдера Olymp Trade - 50$ и 2 дня торговли на платформе.');
             moveScreen();
-        } else if (!goal.checked == false && goal.id =='car') {
+        } else if (goal.id =='iphone' && goalsChecked[1].checked  && goalsChecked[2].checked == false) {
+            showContent.clear();
             showContent.open();
-            showContent.setChoise('audi');
-            showContent.setPrize('audi');
-            showContent.setText('auditext');
+            showContent.setChoise('Iphone Xs - Цена 1000$');
+            showContent.setChoiseSc('Audi A5 - Цена 30000$');
+            showContent.setPrize('31000$');
+            showContent.setText('Цена для трейдера Olymp Trade - 50$ и 3 часа торговли каждый вечер.');
             moveScreen();
-        } else if (!goal.checked == false && goal.id =='palm') {
+        } else if (goal.id =='iphone' && goalsChecked[1].checked  && goalsChecked[2].checked) {
+            showContent.clear();
             showContent.open();
-            showContent.setChoise('palm');
-            showContent.setPrize('palm');
-            showContent.setText('palm');
+            showContent.setChoise('Iphone Xs - Цена 1000$');
+            showContent.setChoiseSc('Audi A5 - Цена 30000$');
+            showContent.setChoiseTrd('Финансовая независимость - бесценно');
+            showContent.setPrize('');
+            showContent.setText('Цена для трейдера Olymp Trade - 50$ и занятие любимым делом в любой точке мира всю вашу жизнь.');
             moveScreen();
-        }
-        if (!goal.checked == false && goal.id =='palm' && goalscheck[0].checked) {
+        } else if (goal.id =='iphone' && goalsChecked[1].checked  == false  && goalsChecked[2].checked) {
+            showContent.clear();
             showContent.open();
-            showContent.setChoise('palm+iphone');
-            showContent.setPrize('palm+iphone');
-            showContent.setText('palm+iphone');
+            showContent.setChoise('Iphone Xs - Цена 1000$');
+            showContent.setChoiseSc('Финансовая независимость - бесценно');
+            showContent.setPrize('');
+            showContent.setText('Цена для трейдера Olymp Trade - 50$ и занятие любимым делом в любой точке мира всю вашу жизнь.');
             moveScreen();
+        } else if (goal.id =='car' && goalsChecked[0].checked == false  && goalsChecked[2].checked == false) {
+            showContent.clear();
+            showContent.open();
+            showContent.setChoise('Audi A5');
+            showContent.setPrize('Цена 30000$');
+            showContent.setText('Цена для трейдера Olymp Trade - 50$ и 2 часа торговли каждый вечер.');
+            moveScreen();
+        } else if (goal.id =='car' && goalsChecked[0].checked && goalsChecked[2].checked == false) {
+            showContent.clear();
+            showContent.open();
+            showContent.setChoise('Iphone Xs - Цена 1000$');
+            showContent.setChoiseSc('Audi A5 - Цена 30000$');
+            showContent.setPrize('31000$');
+            showContent.setText('Цена для трейдера Olymp Trade - 50$ и занятие любимым делом в любой точке мира всю вашу жизнь.');
+            moveScreen();
+        } else if (goal.id =='car' && goalsChecked[0].checked && goalsChecked[2].checked ) {
+            showContent.clear();
+            showContent.open();
+            showContent.setChoise('Iphone Xs - Цена 1000$');
+            showContent.setChoiseSc('Audi A5 - Цена 30000$');
+            showContent.setChoiseTrd('Финансовая независимость - бесценно');
+            showContent.setPrize('');
+            showContent.setText('Цена для трейдера Olymp Trade - 50$ и занятие любимым делом в любой точке мира всю вашу жизнь.');
+            moveScreen();
+        } else if (goal.id =='car' && goalsChecked[0].checked == false && goalsChecked[2].checked ) {
+            showContent.clear();
+            showContent.open();
+            showContent.setChoise('Audi A5 - Цена 30000$');
+            showContent.setChoiseSc('Финансовая независимость - бесценно');
+            showContent.setPrize('');
+            showContent.setText('Цена для трейдера Olymp Trade - 50$ и 2 часа торговли каждый вечер.');
+            moveScreen();
+        } else if (goal.id =='palm' && goalsChecked[0].checked == false && goalsChecked[1].checked == false ) {
+            showContent.clear();
+            showContent.open();
+            showContent.setChoise('Финансовая независимость');
+            showContent.setPrize('');
+            showContent.setText('Цена для трейдера Olymp Trade - 50$ и занятие любимым делом в любой точке мира всю вашу жизнь');
+            moveScreen();
+        } else if (goal.id =='palm' && goalsChecked[0].checked  && goalsChecked[1].checked == false ) {
+            showContent.clear();
+            showContent.open();
+            showContent.setChoise('Iphone Xs - Цена 1000$');
+            showContent.setChoiseSc('Финансовая независимость - бесценно');
+            showContent.setPrize('');
+            showContent.setText('Цена для трейдера Olymp Trade - 50$ и занятие любимым делом в любой точке мира всю вашу жизнь.');
+            moveScreen();
+        } else if (goal.id =='palm' && goalsChecked[0].checked  && goalsChecked[1].checked ) {
+            showContent.clear();
+            showContent.open();
+            showContent.setChoise('Iphone Xs - Цена 1000$');
+            showContent.setChoiseSc('Audi A5 - Цена 30000$');
+            showContent.setChoiseTrd('Финансовая независимость - бесценно');
+            showContent.setPrize('');
+            showContent.setText('Цена для трейдера Olymp Trade - 50$ и занятие любимым делом в любой точке мира всю вашу жизнь.');
+            moveScreen();
+        } else if (goal.id =='palm' && goalsChecked[0].checked == false  && goalsChecked[1].checked ) {
+            showContent.clear();
+            showContent.open();
+            showContent.setChoise('Audi A5 - Цена 30000$');
+            showContent.setChoiseSc('Финансовая независимость - бесценно');
+            showContent.setPrize('');
+            showContent.setText('Цена для трейдера Olymp Trade - 50$ и 2 часа торговли каждый вечер.');
+            moveScreen();
+        } else if (goal.id =='iphone' || goal.id =='car' || goal.id =='palm') {
+            showContent.clear();
         }
     });
 });
 
-
-function openHiddenContent() {
+function createContent() {
 
     const container = document.createElement('div');
 
     container.className = 'content-template';
-    container.innerHTML = template;
+    container.innerHTML =     
+    `<div class="content__wrap">
+        <div class="content__col content__col-left">
+            <ul class="content__list">
+                <li class="content__choise"></li>
+                <li class="content__choise-sc"></li>
+                <li class="content__choise-trd"></li>
+                <li class="content__prize"></li>
+            </ul>
+        </div>
+        <div class="content__col content__col-right">
+            <div class="content__text"></div>
+        </div>
+    </div>`;
 
     let contentChoise = container.querySelector('.content__choise'),
+        contentChoiseSc = container.querySelector('.content__choise-sc'),
+        contentChoiseTrd = container.querySelector('.content__choise-trd'),
         contentPrize = container.querySelector('.content__prize'),
         contentText = container.querySelector('.content__text');
         
     return {
+        clear(){
+            contentChoise.innerHTML = '';
+            contentChoiseSc.innerHTML = '';
+            contentChoiseTrd.innerHTML = '';
+            contentPrize.innerHTML = '';
+            contentText.innerHTML = '';
+        },
         open(){
             content.appendChild(container);
             content.style.display = "";
@@ -68,6 +163,12 @@ function openHiddenContent() {
         },
         setChoise(choise){
             contentChoise.innerHTML = choise;
+        },
+        setChoiseSc(choise){
+            contentChoiseSc.innerHTML = choise;
+        },
+        setChoiseTrd(choise){
+            contentChoiseTrd.innerHTML = choise;
         },
         setPrize(prize){
             contentPrize.innerHTML = prize;
